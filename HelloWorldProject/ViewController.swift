8ba6c05d62f81c4031ed64962900613b85074da2
Sasha <pusmi.1987@gmail.com>
2021-05-18 22:51:22 +1000

    Added elements and action

--- HelloWorldProject/ViewController.swift
+++ HelloWorldProject/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    @IBOutlet weak var showMeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        showMeButton.layer.cornerRadius = 10
     }
 
+    
+    
+    @IBAction func tappedShowMeButton() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            showMeButton.setTitle("Show me", for: .normal)
+        } else {
+            showMeButton.setTitle("Hide me", for: .normal)
+        }
+        
+        
+    }
+    
+    
 
 }
 
